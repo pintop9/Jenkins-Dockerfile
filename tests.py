a3f9e9540370e26ad568afcc711af02511b13ace
pintop9 <idan.pinto12296@gmail.com>
2024-09-28 19:41:09 +0300

    fixed tests

--- tests.py
+++ tests.py
@@ -1,20 +1,20 @@
-    def test_home_status_code(self):
+def test_home_status_code(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
 
-    def test_home_data(self):
+def test_home_data(self):
         response = self.app.get('/')
         self.assertIn(b"Deployment with Jenkins", response.data)
 
-    def test_time_status_code(self):
+def test_time_status_code(self):
         response = self.app.get('/time')
         self.assertEqual(response.status_code, 200)
 
-    def test_time_data(self):
+def test_time_data(self):
         response = self.app.get('/time')
         self.assertIn(b"Current UTC Time", response.data)
 
-    def test_unknown_page(self):
+def test_unknown_page(self):
         response = self.app.get('/unknown')
         self.assertEqual(response.status_code, 404)
 
